5f58cf1a1c61d68e755548f3fc1adcf07325e398
James <44986932+JamesNZL@users.noreply.github.com>
2022-03-04 20:18:20 +1300

    fix(extension): :bug: fix injection of content script

--- src/parseAssignments.ts
+++ src/parseAssignments.ts
@@ -24,69 +24,80 @@ interface Constants {
 	};
 }
 
-const classSelector = (className: string): string => `.${className}`;
+const parseAssignments = () => {
+	const classSelector = (className: string): string => `.${className}`;
 
-const CONSTANTS: Constants = {
-	COURSE: '****** ***',
-	CLASSES: {
-		ASSIGNMENT: 'assignment',
-		TITLE: 'ig-title',
-		AVAILABLE_DATE: 'assignment-date-available',
-		AVAILABLE_STATUS: 'status-description',
-		DUE_DATE: 'assignment-date-due',
-		SCREENREADER_ONLY: 'screenreader-only',
-	},
-	SELECTORS: {
-		get AVAILABLE_STATUS() { return `${classSelector(CONSTANTS.CLASSES.AVAILABLE_DATE)} ${classSelector(CONSTANTS.CLASSES.AVAILABLE_STATUS)}`; },
-		get AVAILABLE_DATE() { return `${classSelector(CONSTANTS.CLASSES.AVAILABLE_DATE)} ${classSelector(CONSTANTS.CLASSES.SCREENREADER_ONLY)}`; },
-		get DUE_DATE() { return `${classSelector(CONSTANTS.CLASSES.DUE_DATE)} ${classSelector(CONSTANTS.CLASSES.SCREENREADER_ONLY)}`; },
-	},
-	VALUES: {
-		NOT_AVAILABLE_STATUS: 'Not available until',
-	},
-};
-
-const verifySelector = (assignment: NonNullable<ReturnType<Element['querySelector']>>, selector: string): NonNullable<ReturnType<Element['querySelector']>> | void => {
-	const element = assignment.querySelector(selector);
-
-	return (element)
-		? element
-		: console.error(`Incorrect selector: ${selector}`);
-};
-
-const parseAvailableDate = (assignment: NonNullable<ReturnType<Element['querySelector']>>): string => {
-	const availableStatus = assignment.querySelector(CONSTANTS.SELECTORS.AVAILABLE_STATUS);
-	const availableDate = assignment.querySelector(CONSTANTS.SELECTORS.AVAILABLE_DATE);
-
-	// If the AVAILABLE_STATUS class actually contains the 'available until' date, return an empty string
-	if (availableStatus?.textContent?.trim() !== CONSTANTS.VALUES.NOT_AVAILABLE_STATUS) return '';
-
-	return availableDate?.textContent?.trim() ?? '';
-};
-
-const parseAssignment = (assignment: NonNullable<ReturnType<Element['querySelector']>>): InputAssignment | void => {
-	const assignmentTitle = verifySelector(assignment, classSelector(CONSTANTS.CLASSES.TITLE));
-
-	// Ensure the configured selectors are valid
-	if (!assignmentTitle?.textContent || !(assignmentTitle instanceof HTMLAnchorElement)) return;
-
-	return {
-		name: assignmentTitle.textContent.trim(),
-		course: CONSTANTS.COURSE,
-		url: assignmentTitle.href,
-		available: parseAvailableDate(assignment),
-		due: assignment.querySelector(CONSTANTS.SELECTORS.DUE_DATE)?.textContent?.trim() ?? '',
+	const CONSTANTS: Constants = {
+		COURSE: '****** ***',
+		CLASSES: {
+			ASSIGNMENT: 'assignment',
+			TITLE: 'ig-title',
+			AVAILABLE_DATE: 'assignment-date-available',
+			AVAILABLE_STATUS: 'status-description',
+			DUE_DATE: 'assignment-date-due',
+			SCREENREADER_ONLY: 'screenreader-only',
+		},
+		SELECTORS: {
+			get AVAILABLE_STATUS() { return `${classSelector(CONSTANTS.CLASSES.AVAILABLE_DATE)} ${classSelector(CONSTANTS.CLASSES.AVAILABLE_STATUS)}`; },
+			get AVAILABLE_DATE() { return `${classSelector(CONSTANTS.CLASSES.AVAILABLE_DATE)} ${classSelector(CONSTANTS.CLASSES.SCREENREADER_ONLY)}`; },
+			get DUE_DATE() { return `${classSelector(CONSTANTS.CLASSES.DUE_DATE)} ${classSelector(CONSTANTS.CLASSES.SCREENREADER_ONLY)}`; },
+		},
+		VALUES: {
+			NOT_AVAILABLE_STATUS: 'Not available until',
+		},
 	};
+
+	const verifySelector = (assignment: NonNullable<ReturnType<Element['querySelector']>>, selector: string): NonNullable<ReturnType<Element['querySelector']>> | void => {
+		const element = assignment.querySelector(selector);
+
+		return (element)
+			? element
+			: console.error(`Incorrect selector: ${selector}`);
+	};
+
+	const parseAvailableDate = (assignment: NonNullable<ReturnType<Element['querySelector']>>): string => {
+		const availableStatus = assignment.querySelector(CONSTANTS.SELECTORS.AVAILABLE_STATUS);
+		const availableDate = assignment.querySelector(CONSTANTS.SELECTORS.AVAILABLE_DATE);
+
+		// If the AVAILABLE_STATUS class actually contains the 'available until' date, return an empty string
+		if (availableStatus?.textContent?.trim() !== CONSTANTS.VALUES.NOT_AVAILABLE_STATUS) return '';
+
+		return availableDate?.textContent?.trim() ?? '';
+	};
+
+	const parseAssignment = (assignment: NonNullable<ReturnType<Element['querySelector']>>): InputAssignment | void => {
+		const assignmentTitle = verifySelector(assignment, classSelector(CONSTANTS.CLASSES.TITLE));
+
+		// Ensure the configured selectors are valid
+		if (!assignmentTitle?.textContent || !(assignmentTitle instanceof HTMLAnchorElement)) return;
+
+		return {
+			name: assignmentTitle.textContent.trim(),
+			course: CONSTANTS.COURSE,
+			url: assignmentTitle.href,
+			available: parseAvailableDate(assignment),
+			due: assignment.querySelector(CONSTANTS.SELECTORS.DUE_DATE)?.textContent?.trim() ?? '',
+		};
+	};
+
+	const assignments = document.getElementsByClassName(CONSTANTS.CLASSES.ASSIGNMENT);
+
+	const parsed = Object.values(assignments).map(assignment => parseAssignment(assignment));
+
+	console.log(JSON.stringify(parsed));
 };
 
 const parseButton = document.getElementById('parseButton');
 
 if (parseButton) {
-	parseButton.addEventListener('click', () => {
-		const assignments = document.getElementsByClassName(CONSTANTS.CLASSES.ASSIGNMENT);
+	parseButton.addEventListener('click', async () => {
+		// @ts-ignore
+		const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-		const parsed = Object.values(assignments).map(assignment => parseAssignment(assignment));
-
-		console.log(parsed);
+		// @ts-ignore
+		chrome.scripting.executeScript({
+			target: { tabId: tab.id },
+			function: parseAssignments,
+		});
 	});
 }
