20b51db60a25261b1766302d09d7205c7f10ad3b
James <44986932+JamesNZL@users.noreply.github.com>
2022-04-07 01:37:16 +1200

    build(gulp): :hammer: create `gulpfile` and update build script

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const { src, dest, parallel } = require('gulp');
+
+const browserify = require('browserify');
+const tsify = require('tsify');
+const source = require('vinyl-source-stream');
+
+const globs = {
+	map(key, func) {
+		return this[key].map(func);
+	},
+	markup: ['src/*.html'],
+	style: ['src/*.css'],
+	scripts: ['src/extension.ts', 'src/parseAssignments.ts', 'src/options.ts'],
+};
+
+function copy(glob) {
+	return function copyGlob() {
+		return src(glob).pipe(dest('dist'));
+	};
+}
+
+function bundle(glob) {
+	return function bundleGlob() {
+		return browserify({
+			debug: false,
+			entries: glob,
+		})
+			.plugin(tsify)
+			.plugin('tinyify')
+			.bundle()
+			.pipe(source(`${glob.match(/src\/(.*)\.ts/)[1]}.js`))
+			.pipe(dest('dist'));
+	};
+}
+
+exports.default = parallel(
+	...globs.map('markup', copy),
+	...globs.map('style', copy),
+	...globs.map('scripts', bundle),
+);
