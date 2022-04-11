JamesNZL/notion-assignment-import

commit 039bbea7e7b01a74c69fe13d7c10dd703797ccfd

feat(types): :label: strongly type record keys

src/popup/popup.ts
@@ -3,7 +3,9 @@ import { exportToNotion } from './import';
 
 import { assertHTMLElements } from '../types/utils';
 
-const buttons: Record<string, HTMLElement | null> = {
+type ButtonNames = ['options', 'parse', 'export', 'viewJSON', 'listCourses', 'copyJSON', 'clearStorage'];
+
+const buttons: Record<ButtonNames[number], HTMLElement | null> = {
 	options: document.getElementById('options-button'),
 	parse: document.getElementById('parse-button'),
 	export: document.getElementById('export-button'),

src/types/utils.ts
@@ -1,6 +1,6 @@
 export type valueof<T> = T[keyof T];
 export type ArrayElement<ArrayType extends readonly unknown[]> = ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
 
-export function assertHTMLElements(object: Record<string, HTMLElement | null>): object is Record<string, HTMLElement> {
+export function assertHTMLElements<K>(object: Record<K extends string ? K : never, HTMLElement | null>): object is Record<keyof typeof object, HTMLElement> {
 	return (Object.values(object).every(element => element !== null));
 }

commit 0b90bb46ee2b2d6354eee3fcb94b6b0bf053fb5d

fix(popup): :bug: fix record indexing by id rather than `ButtonName` (introduced in 039bbea)

src/popup/popup.ts
@@ -24,9 +24,9 @@ if (areHTMLElements(buttons)) {
 		DEFAULT: <Record<ButtonNames[number], string>>Object.fromEntries(
 			Object.entries(buttons).map(([buttonName, buttonElement]) => [buttonName, buttonElement.innerHTML]),
 		),
-		reset(button: HTMLElement, delay: number) {
+		reset(name: ButtonNames[number], delay: number) {
 			setTimeout(() => {
-				button.innerHTML = this.DEFAULT[<keyof typeof this.DEFAULT>button.id];
+				buttons[name].innerHTML = this.DEFAULT[name];
 			}, delay);
 		},
 	};
@@ -61,7 +61,7 @@ if (areHTMLElements(buttons)) {
 		updateSavedCoursesList();
 		if (courseCode) {
 			buttons.parse.innerHTML = `Saved ${courseCode}!`;
-			BUTTON_TEXT.reset(buttons.parse, 1325);
+			BUTTON_TEXT.reset('parse', 1325);
 		}
 	});
 
@@ -76,7 +76,7 @@ if (areHTMLElements(buttons)) {
 				: '';
 
 			buttons.export.innerHTML = `Imported ${createdAssignments.length} assignments!`;
-			BUTTON_TEXT.reset(buttons.export, 3500);
+			BUTTON_TEXT.reset('export', 3500);
 			alert(`Created ${createdAssignments.length} new assignments.${createdNames}`);
 		}
 	});
@@ -102,7 +102,7 @@ if (areHTMLElements(buttons)) {
 		await navigator.clipboard.writeText(JSON.stringify(savedAssignments));
 
 		buttons.copyJSON.innerHTML = 'Copied to clipboard!';
-		BUTTON_TEXT.reset(buttons.copyJSON, 1325);
+		BUTTON_TEXT.reset('copyJSON', 1325);
 	});
 
 	buttons.clearStorage.addEventListener('click', () => {
@@ -111,7 +111,7 @@ if (areHTMLElements(buttons)) {
 		if (buttons.clearStorage.innerHTML !== verifyPrompt) {
 			buttons.clearStorage.innerHTML = verifyPrompt;
 
-			return BUTTON_TEXT.reset(buttons.clearStorage, 1325);
+			return BUTTON_TEXT.reset('clearStorage', 1325);
 		}
 
 		chrome.storage.local.remove('savedAssignments');
@@ -119,7 +119,7 @@ if (areHTMLElements(buttons)) {
 		updateSavedCoursesList();
 
 		buttons.clearStorage.innerHTML = 'Cleared saved assignments!';
-		BUTTON_TEXT.reset(buttons.clearStorage, 3500);
+		BUTTON_TEXT.reset('clearStorage', 3500);
 	});
 }
 