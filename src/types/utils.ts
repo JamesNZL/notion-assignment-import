3cfb436c5c627088a34814f785e87c992e1e7c71
James <44986932+JamesNZL@users.noreply.github.com>
2022-04-12 12:46:14 +1200

    fix(types): :bug: fix `ModifyDeep` giving primitive when modifying a primitive with an object type

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,20 +1,22 @@
 export type valueof<T> = T[keyof T];
 export type ArrayElement<ArrayType extends readonly unknown[]> = ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
 
-export type ModifyDeep<A extends AnyObject, B extends DeepPartialAny<A>> = {
-	[K in keyof A]: B[K] extends never
-	? A[K]
-	: B[K] extends AnyObject
-	? ModifyDeep<A[K], B[K]>
-	: B[K]
-} & (A extends AnyObject ? Omit<B, keyof A> : A);
-
-// makes each property optional and turns each property into any, allowing for type overrides by narrowing any
-type DeepPartialAny<T> = {
-	[P in keyof T]?: T[P] extends AnyObject ? DeepPartialAny<T[P]> : any;
+// makes each property optional and turns each property into unknown, allowing for type overrides by narrowing unknown
+type PartialAny<T> = {
+	[P in keyof T]?: unknown;
 };
 
-type AnyObject = Record<string, any>;
+export type ModifyDeep<A, B> = A extends object
+	? B extends PartialAny<A>
+	? {
+		[K in keyof A]: K extends keyof B
+		? B[K] extends object
+		? ModifyDeep<A[K], B[K]>
+		: B[K]
+		: A[K]
+	} & (A extends object ? Omit<B, keyof A> : A)
+	: A & B
+	: B;
 
 export function areHTMLElements<K extends string>(object: Record<K, HTMLElement | null>): object is Record<K, HTMLElement> {
 	return (Object.values(object).every(element => element !== null));
