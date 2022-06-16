6c6d3d0a41f28697ed8eef5fb91ac4e6b71d6798
James <james@jamesnzl.xyz>
2022-06-16 16:21:10 +1200

    refactor(popup): :art: refactor `popup.ts` with `Button` class

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -3,7 +3,7 @@ import browser from 'webextension-polyfill';
 import { SavedAssignments } from './parse';
 import { exportToNotion } from './import';
 
-import { valueof, areHTMLElements } from '../types/utils';
+import { valueof } from '../types/utils';
 
 // if an id ever changes in HTML, it must be updated here
 // static type checking will then be available through ElementId
@@ -30,149 +30,172 @@ function getElementById(id: ElementId): HTMLElement | null {
 	return document.getElementById(id);
 }
 
-const buttons: Record<ButtonName, ReturnType<typeof getElementById>> = {
-	options: getElementById('options-button'),
-	parse: getElementById('parse-button'),
-	export: getElementById('export-button'),
-	listAssignments: getElementById('list-assignments-button'),
-	listCourses: getElementById('list-courses-button'),
-	copyJSON: getElementById('copy-json-button'),
-	clearStorage: getElementById('clear-storage-button'),
+class Button {
+	private button: HTMLElement;
+	private defaultHtml: string;
+
+	public constructor(id: ElementId) {
+		const element = getElementById(id);
+
+		if (!element) throw new Error(`Invalid button identifier ${id}!`);
+
+		this.button = element;
+		this.defaultHtml = element.innerHTML;
+	}
+
+	public getHtml() {
+		return this.button.innerHTML;
+	}
+
+	public setHtml(html: string) {
+		this.button.innerHTML = html;
+	}
+
+	public resetHtml(delay: number) {
+		setTimeout(() => {
+			this.setHtml(this.defaultHtml);
+		}, delay);
+	}
+
+	public hide() {
+		this.button.style.display = 'none';
+	}
+
+	public unhide() {
+		this.button.style.display = '';
+	}
+
+	public addEventListener(...args: Parameters<typeof HTMLElement.prototype.addEventListener>) {
+		this.button.addEventListener(...args);
+	}
+}
+
+const buttons: Record<ButtonName, Button> = {
+	options: new Button('options-button'),
+	parse: new Button('parse-button'),
+	export: new Button('export-button'),
+	listAssignments: new Button('list-assignments-button'),
+	listCourses: new Button('list-courses-button'),
+	copyJSON: new Button('copy-json-button'),
+	clearStorage: new Button('clear-storage-button'),
 };
 
-function setButtonDisplay(button: HTMLElement | null, display: 'none' | 'inline-block') {
-	if (button instanceof HTMLElement) button.style.display = display;
-}
+buttons.options.addEventListener('click', () => {
+	if (browser.runtime.openOptionsPage) {
+		browser.runtime.openOptionsPage();
+	}
 
-if (areHTMLElements(buttons)) {
-	const BUTTON_TEXT = {
-		DEFAULT: <Record<ButtonId, string>>Object.fromEntries(
-			Object.values(buttons).map(button => [<ButtonId>button.id, button.innerHTML]),
-		),
-		reset(button: valueof<typeof buttons>, delay: number) {
-			setTimeout(() => {
-				button.innerHTML = this.DEFAULT[<ButtonId>button.id];
-			}, delay);
-		},
-	};
+	else {
+		window.open(browser.runtime.getURL('options.html'));
+	}
+});
 
-	buttons.options.addEventListener('click', () => {
-		if (browser.runtime.openOptionsPage) {
-			browser.runtime.openOptionsPage();
-		}
+buttons.parse.addEventListener('click', async () => {
+	await browser.storage.local.remove('savedCourse');
 
-		else {
-			window.open(browser.runtime.getURL('options.html'));
-		}
-	});
+	const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
 
-	buttons.parse.addEventListener('click', async () => {
-		await browser.storage.local.remove('savedCourse');
+	if (!tab.id) return alert('No active tab found.');
 
-		const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
-
-		if (!tab.id) return alert('No active tab found.');
-
-		const result: unknown = await (
-			(browser.scripting)
-				? browser.scripting.executeScript({
-					target: { tabId: tab.id },
-					files: ['popup/parse.js'],
+	const result: unknown = await (
+		(browser.scripting)
+			? browser.scripting.executeScript({
+				target: { tabId: tab.id },
+				files: ['popup/parse.js'],
+			})
+			: browser.tabs.executeScript(tab.id, {
+				file: '/popup/parse.js',
+			})
+				.catch((error: Error) => {
+					// Ignore non-structured-clonable error
+					if (error.message.includes('non-structured-clonable')) return true;
+					throw error;
 				})
-				: browser.tabs.executeScript(tab.id, {
-					file: '/popup/parse.js',
-				})
-					.catch((error: Error) => {
-						// Ignore non-structured-clonable error
-						if (error.message.includes('non-structured-clonable')) return true;
-						throw error;
-					})
-		)
-			.catch(console.error);
+	)
+		.catch(console.error);
 
-		if (!result) return alert('An error was encountered whilst attempting to parse assignments.');
+	if (!result) return alert('An error was encountered whilst attempting to parse assignments.');
 
-		let courseCode = undefined;
-		while (courseCode === undefined) {
-			({ savedCourse: courseCode } = await browser.storage.local.get('savedCourse'));
-		}
+	let courseCode = undefined;
+	while (courseCode === undefined) {
+		({ savedCourse: courseCode } = await browser.storage.local.get('savedCourse'));
+	}
 
-		updateSavedCoursesList();
-		if (courseCode) {
-			buttons.parse.innerHTML = `Saved ${courseCode}!`;
-			BUTTON_TEXT.reset(buttons.parse, 1325);
-		}
-	});
+	updateSavedCoursesList();
+	if (courseCode) {
+		buttons.parse.setHtml(`Saved ${courseCode}!`);
+		buttons.parse.resetHtml(1325);
+	}
+});
 
-	buttons.export.addEventListener('click', async () => {
-		buttons.export.innerHTML = 'Exporting to Notion...';
+buttons.export.addEventListener('click', async () => {
+	buttons.export.setHtml('Exporting to Notion...');
 
-		const createdAssignments = await exportToNotion();
+	const createdAssignments = await exportToNotion();
 
-		if (createdAssignments) {
-			const createdNames = (createdAssignments.length)
-				? createdAssignments.reduce((list, { course, name }, index) => list + `${index + 1}. ${course} ${name}\n`, '\n\n')
-				: '';
+	if (createdAssignments) {
+		const createdNames = (createdAssignments.length)
+			? createdAssignments.reduce((list, { course, name }, index) => list + `${index + 1}. ${course} ${name}\n`, '\n\n')
+			: '';
 
-			buttons.export.innerHTML = `Imported ${createdAssignments.length} assignments!`;
-			BUTTON_TEXT.reset(buttons.export, 3500);
-			alert(`Created ${createdAssignments.length} new assignments.${createdNames}`);
-		}
-	});
+		buttons.export.setHtml(`Imported ${createdAssignments.length} assignments!`);
+		buttons.export.resetHtml(3500);
+		alert(`Created ${createdAssignments.length} new assignments.${createdNames}`);
+	}
+});
 
-	buttons.listAssignments.addEventListener('click', async () => {
-		const savedCourses = getElementById('saved-courses-list');
+buttons.listAssignments.addEventListener('click', async () => {
+	const savedCourses = getElementById('saved-courses-list');
 
-		if (savedCourses) {
-			const { savedAssignments } = <{ savedAssignments: SavedAssignments; }>await browser.storage.local.get({ savedAssignments: {} });
-
-			const assignmentsList = Object.entries(savedAssignments)
-				.reduce((list: string, [course, assignments]) => {
-					return list + `<li>${course}</li>\n
-					<ul>
-					${assignments.reduce((courseList: string, { icon, name, url }) => {
-						return courseList + `<li>${(icon) ? `${icon} ` : ''}<a href='${url}' target='_blank'>${name}</a></li>\n`;
-					}, '')}
-					</ul>\n`;
-				}, '');
-
-			setButtonDisplay(buttons.listAssignments, 'none');
-			setButtonDisplay(buttons.listCourses, 'inline-block');
-
-			savedCourses.innerHTML = (assignmentsList)
-				? `<ol>${assignmentsList}</ol>`
-				: '<p>No saved assignments.</p>';
-		}
-	});
-
-	buttons.listCourses.addEventListener('click', updateSavedCoursesList);
-
-	buttons.copyJSON.addEventListener('click', async () => {
+	if (savedCourses) {
 		const { savedAssignments } = <{ savedAssignments: SavedAssignments; }>await browser.storage.local.get({ savedAssignments: {} });
 
-		await navigator.clipboard.writeText(JSON.stringify(savedAssignments));
+		const assignmentsList = Object.entries(savedAssignments)
+			.reduce((list: string, [course, assignments]) => {
+				return list + `<li>${course}</li>\n
+					<ul>
+					${assignments.reduce((courseList: string, { icon, name, url }) => {
+					return courseList + `<li>${(icon) ? `${icon} ` : ''}<a href='${url}' target='_blank'>${name}</a></li>\n`;
+				}, '')}
+					</ul>\n`;
+			}, '');
 
-		buttons.copyJSON.innerHTML = 'Copied to clipboard!';
-		BUTTON_TEXT.reset(buttons.copyJSON, 1325);
-	});
+		buttons.listAssignments.hide();
+		buttons.listCourses.unhide();
 
-	buttons.clearStorage.addEventListener('click', () => {
-		const verifyPrompt = 'I\'m sure!';
+		savedCourses.innerHTML = (assignmentsList)
+			? `<ol>${assignmentsList}</ol>`
+			: '<p>No saved assignments.</p>';
+	}
+});
 
-		if (buttons.clearStorage.innerHTML !== verifyPrompt) {
-			buttons.clearStorage.innerHTML = verifyPrompt;
+buttons.listCourses.addEventListener('click', updateSavedCoursesList);
 
-			return BUTTON_TEXT.reset(buttons.clearStorage, 1325);
-		}
+buttons.copyJSON.addEventListener('click', async () => {
+	const { savedAssignments } = <{ savedAssignments: SavedAssignments; }>await browser.storage.local.get({ savedAssignments: {} });
 
-		browser.storage.local.remove('savedAssignments');
+	await navigator.clipboard.writeText(JSON.stringify(savedAssignments));
 
-		updateSavedCoursesList();
+	buttons.copyJSON.setHtml('Copied to clipboard!');
+	buttons.copyJSON.resetHtml(1325);
+});
 
-		buttons.clearStorage.innerHTML = 'Cleared saved assignments!';
-		BUTTON_TEXT.reset(buttons.clearStorage, 3500);
-	});
-}
+buttons.clearStorage.addEventListener('click', () => {
+	const verifyPrompt = 'I\'m sure!';
+
+	if (buttons.clearStorage.getHtml() !== verifyPrompt) {
+		buttons.clearStorage.setHtml(verifyPrompt);
+
+		return buttons.clearStorage.resetHtml(1325);
+	}
+
+	browser.storage.local.remove('savedAssignments');
+
+	updateSavedCoursesList();
+
+	buttons.clearStorage.setHtml('Cleared saved assignments!');
+	buttons.clearStorage.resetHtml(3500);
+});
 
 async function updateSavedCoursesList() {
 	const savedCourses = getElementById('saved-courses-list');
@@ -182,8 +205,8 @@ async function updateSavedCoursesList() {
 
 		const coursesList = Object.entries(savedAssignments).reduce((list: string, [course, assignments]) => list + `<li>${course} (<code>${assignments.length}</code> assignments)</li>\n`, '');
 
-		setButtonDisplay(buttons.listCourses, 'none');
-		setButtonDisplay(buttons.listAssignments, 'inline-block');
+		buttons.listCourses.hide();
+		buttons.listAssignments.unhide();
 
 		savedCourses.innerHTML = (coursesList)
 			? `<ol>${coursesList}</ol>`
