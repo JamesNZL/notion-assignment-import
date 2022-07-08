d72d654ddae6670bf6a01cc571a493531288a854
James <james@jamesnzl.xyz>
2022-07-08 20:22:44 +1200

    refactor(import): :fire: remove unused methods

--- src/popup/import.ts
+++ src/popup/import.ts
@@ -119,31 +119,7 @@ export async function exportToNotion(): Promise<void | IParsedAssignment[]> {
 			this.assignment = assignment;
 		}
 
-		public get name(): string {
-			return NotionClient.resolveTitle(this.assignment) ?? '';
-		}
-
-		public get course(): string | undefined {
-			try {
-				if (!options.propertyNames.course) throw null;
-
-				if (!('properties' in this.assignment) || !(options.propertyNames.course in this.assignment.properties)) throw null;
-
-				// Extract the course property from the page
-				const courseProperty = this.assignment.properties[options.propertyNames.course];
-
-				if (!('select' in courseProperty)) throw null;
-
-				// If the course property is a select property, return its name
-				return courseProperty.select?.name;
-			}
-			catch {
-				// Return undefined if no select was found
-				return undefined;
-			}
-		}
-
-		public get url(): string | undefined {
+		public get url() {
 			try {
 				if (!options.propertyNames.url) throw null;
 
