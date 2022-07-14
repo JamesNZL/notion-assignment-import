fab3a05f95dad042b568a4241c282b6a9d06c09d
James <james@jamesnzl.xyz>
2022-07-14 21:31:12 +1200

    feat(elements): :sparkles: implement bones of `SegmentedControl`

--- src/elements/SegmentedControl.ts
+++ src/elements/SegmentedControl.ts
@@ -1,15 +1,103 @@
 import { Element } from './Element';
+import { Input } from './Input';
 
 import { SupportedTypes } from '../options/configuration';
-import { InputFieldValidator } from '../options/validator';
+
+interface Segment {
+	id: string;
+	// TODO: am i able to make this a generic class, and make getInstance play nicely?
+	value: SupportedTypes;
+	default: boolean;
+	showDependents: boolean;
+}
+
+type Segments = (
+	Segment & {
+		input: Input;
+	}
+)[];
 
 export class SegmentedControl extends Element {
-	getValue(): SupportedTypes;
-	validate(force?: boolean): Promise<SupportedTypes | typeof InputFieldValidator.INVALID_INPUT>;
-	setValue(value: SupportedTypes, dispatchEvent?: boolean): void;
-	show(): void;
-	hide(): void;
-	toggleDependents(dependents: readonly string[]): void;
-	addEventListener(...args: Parameters<typeof HTMLElement.prototype.addEventListener>): void;
-	dispatchInputEvent(bubbles?: boolean): void;
+	private segments: Segments;
+
+	private constructor(id: string, type = 'segmented control', segments: Segment[]) {
+		super(id, type);
+
+		this.segments = segments.map(segment => ({
+			...segment,
+			input: Input.getInstance(segment.id, 'segmented control input'),
+		}));
+	}
+
+	public static override getInstance<T extends string>(id: T, type = 'segmented control', segments?: Segment[]): SegmentedControl {
+		if (!segments) throw new Error(`You must declare the segments for this SegmentedControl ${id}!`);
+
+		return SegmentedControl.instances[id] = (SegmentedControl.instances[id] instanceof SegmentedControl)
+			? <SegmentedControl>SegmentedControl.instances[id]
+			: new SegmentedControl(id, type, segments);
+	}
+
+	public async validate() {
+		return await this.getValue();
+	}
+
+	private getCheckedSegment() {
+		return this.element.querySelector('input:checked');
+	}
+
+	public getValue(): SupportedTypes {
+		const checkedSegment = this.getCheckedSegment();
+
+		if (!checkedSegment) return this.segments.find(segment => Boolean(segment.default))?.value ?? null;
+
+		return this.segments.find(({ id }) => id === checkedSegment.id)?.value ?? null;
+	}
+
+	// TODO: see generics above
+	public setValue(value: SupportedTypes, dispatchEvent = true) {
+		const segmentToCheck = this.segments.find(segment => segment.value === value);
+
+		if (!segmentToCheck) throw new Error(`Failed to set unexpected value ${value} of type ${typeof value} on segmented control ${this.id}`);
+
+		segmentToCheck.input.setValue(true);
+
+		if (!dispatchEvent) return;
+		this.dispatchInputEvent();
+	}
+
+	public override show() {
+		super.show();
+		this.dispatchInputEvent();
+	}
+
+	public override hide() {
+		super.hide();
+		this.dispatchInputEvent();
+	}
+
+	public dispatchInputEvent(bubbles?: boolean) {
+		this.dispatchEvent(new Event('input', { bubbles }));
+	}
+
+	public toggleDependents(dependents: readonly string[]) {
+		const checkedSegment = this.getCheckedSegment();
+
+		const showDependents = this.segments.find(({ id }) => id === checkedSegment?.id)?.showDependents;
+
+		if (!checkedSegment || this.isHidden() || !showDependents) {
+			dependents.forEach(dependentId => {
+				const dependent = Element.getInstance(dependentId, 'dependent');
+				dependent.hide();
+				dependent.dispatchEvent(new Event('input', { bubbles: true }));
+			});
+
+			return;
+		}
+
+		dependents.forEach(dependentId => {
+			const dependent = Element.getInstance(dependentId, 'dependent');
+			dependent.show();
+			dependent.dispatchEvent(new Event('input', { bubbles: true }));
+		});
+	}
 }
